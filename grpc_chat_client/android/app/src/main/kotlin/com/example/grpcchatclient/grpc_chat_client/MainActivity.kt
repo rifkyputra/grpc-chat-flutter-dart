5a9e43159c517d4ae0686206de6de6c3dea7aab6
rifky <rifqempul@gmail.com>
2022-04-17 10:37:41 +0700

    initialize

---
+++ grpc_chat_client/android/app/src/main/kotlin/com/example/grpcchatclient/grpc_chat_client/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.grpcchatclient.grpc_chat_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ grpc_chat_client/lib/src/cubit/message_send_cubit/message_send_cubit.dart
@@ -0,0 +1,3 @@
+class MessageSendCubit extends Cubit<> {
+  
+}
